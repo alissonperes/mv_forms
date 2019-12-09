74c9c38e2fb31e994ac2fd3d3f3f124a4ae95bfe
David Elì <davidelimg1@gmail.com>
2019-12-09 11:57:07 -0600

    Edit form and methods controllerd added and fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,17 +16,16 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    @user = User.find(params[:id])
+  end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
     end
   end
 
